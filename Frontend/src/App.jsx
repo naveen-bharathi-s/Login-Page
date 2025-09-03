fb80f8dee095a0314fec42b526f35bfd005238d1
naveen-bharathi-s <naveenbharathi555@gmail.com>
2025-09-03 13:49:59 +0530

    link updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,6 +16,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate, Link } from 'react-router-dom'
+import Success from './pages/success.jsx'
 
 function App() {
   const [email, setEmail] = useState("")
